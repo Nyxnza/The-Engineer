4c7435dd3152594a02ff5e5814feb8de9deed0e5
Nyxnzna <fleancediesta@gmail.com>
2026-03-30 10:23:36 +0800

    fix config path in mute

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const config = require('../config.json');
+const config = require('../../config.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
